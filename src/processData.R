14beebfc1a5819eab9a61e940f23cb73a53e08b0
Mattijs <m.d.vandekuijlen@students.uu.nl>
2017-10-12 16:53:56 +0200

    Started with reading in all the data

--- src/processData.R
+++ src/processData.R
@@ -1,1 +1,13 @@
-# This file is used for processing the data
+# This file contains functionality for processing the data
+
+# Name: processData
+# Parameters:
+#   * experimentFile(type: character): the path to the file with the experiment data for the corresponding test subject
+#   * participantRatings(type: data.frame): the participant ratings for the test subject for which we need to 
+#                                           process the data
+# Result: A vector of training samples, one training sample for each text that has been read by the test subject
+# Description: this function processes all the experiment data of the test subject
+processData <- function(experimentFile, participantRatings)
+{
+  
+}
--- src/readData.R
+++ src/readData.R
@@ -1,1 +1,55 @@
-# This file is used for reading in all necessary data
+# This file contains functionality for reading in all necessary data
+
+library("xlsx")
+
+# Name: readData
+# Parameters: has no parameters
+# Result: has no return value
+# Description: this function will read in all data and call the appropriate functions for working with this data
+readData <- function()
+{
+  # The path to the data directory, i.e. to the directory where all data on the experiments and on the participant 
+  # ratings can be found
+  dataDirectoryPath <- paste(dirname(getwd()), "/data", sep = "")
+  
+  # List all file names of the experiment data files, i.e. all file names of the data on the eye movements for 
+  # each test subject
+  experimentFiles <- list.files(path = paste(dataDirectoryPath, "/eye-tracking_data", sep = ""), pattern = "*.txt")
+  
+  # Create a data frame for the training data that will be used for training the model
+  trainingData <- data.frame(Fixations = integer(),
+                             Fixations_Progressive_Fraction = double(),
+                             Fixations_Regressive_Fraction = double(),
+                             Fixation_Progressive_Duration_Mean = double(),
+                             Fixation_Regressive_Duration_Mean = double(),
+                             Fixation_Progressive_Duration_SD = double(),
+                             Fixation_Regressive_Duration_SD = double(),
+                             Saccades = integer(),
+                             Saccade_Duration_Mean = double(),
+                             Saccade_Amplitude_Mean = double(),
+                             Saccade_Peak_Velocity_Mean = double(),
+                             Saccade_Duration_SD = double(),
+                             Saccade_Amplitude_SD = double(),
+                             Saccade_Peak_Velocity_SD = double(),
+                             Blinks = integer(),
+                             Blink_Duration_Mean = double(),
+                             Blink_Duration_SD = double(),
+                             Interest = double(),
+                             Novelty_Complexity = double(),
+                             Comprehensibility = double())
+  
+  # Read the data files with participant ratings
+  participantRatings <- read.xlsx(paste(dataDirectoryPath, "/participant_ratings.xlsx", sep = ""), sheetIndex = 1, 
+                                  colIndex = c(1:6))
+  
+  # Loop over all participants to process their eye tracking data one at a time
+  for(index in c(1:length(unique(participantRatings$person.ID))))
+  {
+    # Retrieve the ID of the current participant, i.e. of the current test subject
+    participant <- unique(participantRatings$person.ID)[index]
+    
+    # Process the eye tracking data of the current test subject
+    processData(experimentFile = paste(dataDirectoryPath, "/eye-tracking_data/", experimentFiles[index], sep = ""), 
+                participantRatings = subset(participantRatings, participantRatings$person.ID == participant))
+  }
+}
