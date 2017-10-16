dafda42f14808361bdfc01529b7ebece1a54cf20
Mattijs <m.d.vandekuijlen@students.uu.nl>
2017-10-16 11:08:47 +0200

    Started on filtering the data

--- src/processData.R
+++ src/processData.R
@@ -22,4 +22,24 @@ processData <- function(experimentFile, participantRatings)
   # Remove all samples for which no stimulus_id has been observed, i.e. all samples that do not belong to one of the 
   # 18 texts that the test subject has read
   eyeTrackingData <- subset(eyeTrackingData, eyeTrackingData$stimulus_id %in% participantRatings$text.ID)
+  
+  # Process each text separately
+  for(textID in unique(participantRatings$text.ID))
+  {
+    # Subset the eye tracking data for the current text
+    textData <- subset(eyeTrackingData, eyeTrackingData$stimulus_id == textID)
+    filterData(textData)
+  }
+}
+
+# Name: filterData
+# Parameters:
+#   * textData(type: data.frame): the eye tracking data for a single text and for a single user
+# Result: A data.frame of filtered eye tracking data
+# Description: this function filters the data of a single text that was read by a single user
+filterData <- function(textData)
+{
+  # Filter out all data where at least one of the positions of the eyes has a negative value
+  filteredData <- subset(textData, textData$`L POR X [px]` >= 0 & textData$`L POR Y [px]` >= 0 & 
+                         textData$`R POR X [px]` >= 0 & textData$`R POR Y [px]` >= 0)
 }
